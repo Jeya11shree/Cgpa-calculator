2434b2722baa55777bc8acb767e64073b3f28c9d
Jeya11shree <130202123+Jeya11shree@users.noreply.github.com>
2024-03-27 23:02:56 +0530

    Add files via upload

--- cgpacalculatortask1.py
+++ cgpacalculatortask1.py
@@ -4,29 +4,18 @@ import json
 # Define grade points dictionary
 grade_points ={'O': 10,
  'A+':9,
- 'A': 8,
+ 'A':8,
  'B+':7,
- 'B': 6,
- 'C': 5,
- 'RA': 0,
- 'SA': 0}
+ 'B':6,
+ 'C':5,
+ 'RA':0,
+ 'SA':0}
 
-# Prompt the user to input the filenames separated by commas
 filenames = input("Enter the filenames of the CSV files (separated by commas): ").split(',')
 output = {}
 
-if len(filenames) == 1:
-    try:
-        df = pd.read_csv(filenames[0].strip())
-        
-        gpa = (df['credits'] * df['grade'].map(grade_points)).sum() / df[df['grade'].isin(grade_points.keys())]['credits'].sum()
-        
-        output[filenames[0].strip()] = round(gpa, 2)
-    except FileNotFoundError:
-        output["error"] = f"File '{filenames[0].strip()}' not found."
-    except Exception as e:
-        output["error"] = f"An error occurred while processing '{filenames[0].strip()}': {str(e)}"
-else:
+     
+if len(filenames)>=1:
     total_credits = 0
     total_grade_points = 0
 
@@ -48,8 +37,7 @@ else:
             }
         except FileNotFoundError:
             output[filename.strip()] = f"File '{filename.strip()}' not found"
-        except Exception as e:
-            output[filename.strip()] = f"An error occurred while processing '{filename.strip()}': {str(e)}"
+       
 
     cgpa = round(total_grade_points / total_credits, 2)
     output["CGPA"] = cgpa
